206d2ef4fbcf7140e48fdd7c181e03a12ea965c9
adhe7x <adhe7x.almosteasy@gmail.com>
2022-07-17 17:12:29 +0700

    Simple Navbar

---
+++ src/themes/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme ({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#c71111',
+    },
+    secondary: {
+      main: '#11bb03',
+    },
+    background: {
+      default: '#252424',
+    },
+  },
+});
+
+export default theme;

26bb03a14965ef7084c5f25fffc038d6b1aa3d65
adhe7x <adhe7x.almosteasy@gmail.com>
2022-07-17 19:05:33 +0700

    Simply NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
               letterSpacing: '.3rem',
             }}
           >
-            NONTON
+            Movie App
           </Typography>
           <Box sx={{ display: 'block' }}>
             {navItems.map((item) => (

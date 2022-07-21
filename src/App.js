8981b8fe9a478a76f299bd6b18b887003509bbbe
adhe7x <adhe7x.almosteasy@gmail.com>
2022-07-21 23:59:19 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen';
 
 function App() {
 
   return (
     <div className="app">
-      <Router>
-        <Route path="/" element={<HomeScreen />}>
+      <Routes>
+        <Route path='/' element={<HomeScreen />}>        
         </Route>
-      </Router>
+      </Routes>
     </div>
   );
 }
